c6da784d7fc79c856712b4adb1057433dc82ea99
Daniel G. Taylor <danielgtaylor@gmail.com>
2015-05-14 14:12:06 -0700

    Use babel-runtime instead of core-js

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 var _ = require('lodash');
 
-// Polyfill for some ES6 features like private class symbols
-require('core-js/es6');
-
 module.exports = _.extend(
   require('./lib/primitives'),
   require('./lib/convert')
