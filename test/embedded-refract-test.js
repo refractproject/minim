f679b544a4249cb9af05cda62bce21c00d3597be
Stephen Mizell <smizell@gmail.com>
2015-06-20 08:28:41 -0500

    Add code to convert to embedded Refract

--- test/embedded-refract-test.js
+++ test/embedded-refract-test.js
@@ -190,4 +190,253 @@ describe('Embedded Refract', function() {
       });
     });
   });
+
+  context('when converting to embedded refract', function() {
+    describe('NullElement', function() {
+      context('when no attributes are set', function() {
+        var element = new minim.NullElement();
+
+        it('returns its value only', function() {
+          expect(element.toEmbeddedRefract()).to.equal(null);
+        });
+      });
+
+      context('when attributes have been set', function() {
+        var element = new minim.NullElement(null, {}, { foo: 'bar' });
+
+        it('returns an embedded refract object', function() {
+          expect(element.toEmbeddedRefract()).to.deep.equal({
+            refract: {
+              element: 'null',
+              meta: {},
+              attributes: {
+                foo: 'bar'
+              },
+              content: null
+            }
+          });
+        });
+      });
+    });
+
+    describe('StringElement', function() {
+      context('when no attributes are set', function() {
+        var element = new minim.StringElement('a');
+
+        it('returns its value only', function() {
+          expect(element.toEmbeddedRefract()).to.equal('a');
+        });
+      });
+
+      context('when attributes have been set', function() {
+        var element = new minim.StringElement('a', {}, { foo: 'bar' });
+
+        it('returns an embedded refract object', function() {
+          expect(element.toEmbeddedRefract()).to.deep.equal({
+            refract: {
+              element: 'string',
+              meta: {},
+              attributes: {
+                foo: 'bar'
+              },
+              content: 'a'
+            }
+          });
+        });
+      });
+    });
+
+    describe('NumberElement', function() {
+      context('when no attributes are set', function() {
+        var element = new minim.NumberElement(4);
+
+        it('returns its value only', function() {
+          expect(element.toEmbeddedRefract()).to.equal(4);
+        });
+      });
+
+      context('when attributes have been set', function() {
+        var element = new minim.NumberElement(4, {}, { foo: 'bar' });
+
+        it('returns an embedded refract object', function() {
+          expect(element.toEmbeddedRefract()).to.deep.equal({
+            refract: {
+              element: 'number',
+              meta: {},
+              attributes: {
+                foo: 'bar'
+              },
+              content: 4
+            }
+          });
+        });
+      });
+    });
+
+    describe('BooleanElement', function() {
+      context('when no attributes are set', function() {
+        var element = new minim.BooleanElement(true);
+
+        it('returns its value only', function() {
+          expect(element.toEmbeddedRefract()).to.equal(true);
+        });
+      });
+
+      context('when attributes have been set', function() {
+        var element = new minim.BooleanElement(true, {}, { foo: 'bar' });
+
+        it('returns an embedded refract object', function() {
+          expect(element.toEmbeddedRefract()).to.deep.equal({
+            refract: {
+              element: 'boolean',
+              meta: {},
+              attributes: {
+                foo: 'bar'
+              },
+              content: true
+            }
+          });
+        });
+      });
+    });
+
+    describe('ArrayElement', function() {
+      context('when no attributes are set', function() {
+        var element = new minim.ArrayElement([1, 2]);
+
+        it('returns its value only', function() {
+          expect(element.toEmbeddedRefract()).to.deep.equal([1, 2]);
+        });
+
+        context('when an item has attributes', function() {
+          var element = new minim.ArrayElement([1, 2]);
+          element.first().attributes.set('foo', 'bar');
+
+          it('embeds refract in the item with attributes', function() {
+            expect(element.toEmbeddedRefract()).to.deep.equal([{
+              refract: {
+                element: 'number',
+                meta: {},
+                attributes: { foo: 'bar' },
+                content: 1
+              }
+            }, 2]);
+          });
+        })
+      });
+
+      context('when attributes have been set', function() {
+        var element = new minim.ArrayElement([1, 2], {}, { foo: 'bar' });
+
+        it('returns an embedded refract object', function() {
+          expect(element.toEmbeddedRefract()).to.deep.equal({
+            refract: {
+              element: 'array',
+              meta: {},
+              attributes: {
+                foo: 'bar'
+              },
+              content: [
+                {
+                  element: 'number',
+                  meta: {},
+                  attributes: {},
+                  content: 1
+                },
+                {
+                  element: 'number',
+                  meta: {},
+                  attributes: {},
+                  content: 2
+                }
+              ]
+            }
+          });
+        });
+      });
+    });
+
+    describe('ObjectElement', function() {
+      context('when no attributes are set', function() {
+        var element = new minim.ObjectElement({ a: 1, b: 2 });
+
+        it('returns its value only', function() {
+          expect(element.toEmbeddedRefract()).to.deep.equal({ a: 1, b: 2 });
+        });
+
+        context('when an item has attributes', function() {
+          var element = new minim.ObjectElement({ a: 1, b: 2 });
+          element.get('a').attributes.set('foo', 'bar');
+
+          it('embeds refract in the item with attributes', function() {
+            expect(element.toEmbeddedRefract()).to.deep.equal({
+              a: {
+                refract: {
+                  element: 'number',
+                  meta: {},
+                  attributes: { foo: 'bar' },
+                  content: 1
+                }
+              },
+              b: 2
+            });
+          });
+        })
+      });
+
+      context('when attributes have been set', function() {
+        var element = new minim.ObjectElement({ a: 1, b: 2 }, {}, { foo: 'bar' });
+
+        it('returns an embedded refract object', function() {
+          expect(element.toEmbeddedRefract()).to.deep.equal({
+            refract: {
+              element: 'object',
+              meta: {},
+              attributes: { foo: 'bar' },
+              content: [
+                {
+                  element: 'member',
+                  meta: {},
+                  attributes: {},
+                  content: {
+                    key: {
+                      element: 'string',
+                      meta: {},
+                      attributes: {},
+                      content: 'a'
+                    },
+                    value: {
+                      element: 'number',
+                      meta: {},
+                      attributes: {},
+                      content: 1
+                    }
+                  }
+                },
+                {
+                  element: 'member',
+                  meta: {},
+                  attributes: {},
+                  content: {
+                    key: {
+                      element: 'string',
+                      meta: {},
+                      attributes: {},
+                      content: 'b'
+                    },
+                    value: {
+                      element: 'number',
+                      meta: {},
+                      attributes: {},
+                      content: 2
+                    }
+                  }
+                }
+              ]
+            }
+          });
+        });
+      });
+    });
+  });
 });
