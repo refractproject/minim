798923fcc0533df461e80efb0e3aeba79fd18b2c
Kyle Fuller <kyle@fuller.li>
2019-02-22 16:01:43 +0000

    chore: uninstall unused sinon dependency

--- test/spec-helper.js
+++ test/spec-helper.js
@@ -1,9 +1,3 @@
-require('mocha-sinon');
-
 exports.chai = require('chai');
-exports.sinon = require('sinon');
-exports.sinonChai = require('sinon-chai');
 
-// Configure mocking
-exports.chai.use(exports.sinonChai);
 exports.expect = exports.chai.expect;
